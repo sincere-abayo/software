bfe4de8d013589a3bf1b842ac13b140123ee4e22
sincere-abayo <abayosincere11@gmail.com>
2024-03-21 13:23:56 +0200

    commit this data

---
+++ upload/file.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="" method="post" enctype="multipart/form-data">
+        <input type="file" name="file">
+        <input type="submit" name="submit">
+        <img src="" alt="">
+    </form>
+
+    <?php
+    if (isset($_POST['submit'])) {
+
+        //getting file name
+        // //    echo  $_FILES['file']['name'];
+        //getting file size
+        //    echo  $_FILES['file']['size'];
+
+        // getting file content
+        //    echo  $_FILES['file']['tmp_name'];
+    $dir="images/";
+    $file_name = $_FILES['file']['name'];
+    $file_path= $dir . basename($file_name);
+
+        $info = pathinfo($file_name);
+        $extension = strtolower($info['extension']);
+        // $extension = $info['extension'];
+
+        $allowed_extension = ["jpeg", "jpg", "png", "gif", "tiff", "imap"];
+        $file_size = $_FILES['file']['size'];
+
+        if (in_array($extension, $allowed_extension)) {
+
+            if ($file_size < 1000000)
+             {
+                if (file_exists($file_path)) {
+                    echo "file already exist";
+                }
+                else  {
+                   $done=move_uploaded_file($_FILES['file']['tmp_name'],$file_path);
+                    if ($done) {
+                       echo "file uploaded well";
+                    } else {
+                       echo "failed to upload file";
+                    }
+                    
+                //    echo "file not exist";
+                }
+                // echo "file size ok";
+            } 
+            else {
+                echo "file size is larger than 1MB";
+            }
+        } else {
+
+
+            echo " file not image ";
+        }
+
+        // echo $extension;
+    }
+    ?>
+</body>
+
+</html>
